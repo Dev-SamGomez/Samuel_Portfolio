a31717529d039c84cdceba4874a234ba143b46b8
Samuel Gomez <sgomezp.dev@gmail.com>
2022-08-04 20:39:04 -0600

    La barra de navegacion esta lista

--- JS/main_in.js
+++ JS/main_in.js
@@ -1,15 +1,23 @@
-jQuery('document').ready(function($) {
-    var menuBtn = $('.burguer-menu'),
-        menu = $('.navigation ul');
+const NumberInteractions = 7;
+const Seconds = 0.5;
+const Transition = (i,NumInt,Sc) => {
+     return i / NumInt + Sc;
+}
+const CalcTimeMov = 7 + 1.7;
+const navSlide = () => {
+    const Burguer = document.querySelector('.burguer-menu');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
-    menuBtn.click(function() {
-
-        if (menu.hasClass('show')) {
-            menu.removeClass('show');
-            console.log('Remueve clase');
-        } else {
-            menu.addClass('show');
-            console.log('Agrega clase');
-        }
-    });
-});
+    Burguer.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((item,index) => {
+            item.style.animation ?
+            item.style.animation = '' :
+            item.style.animation = `navLinkMovements 0.5s ease forwards ${Transition(index,NumberInteractions,Seconds)}s`;
+            //console.log(index / CalcTimeMov);
+        });
+        Burguer.classList.toggle('toggle');
+    });     
+}
+navSlide();
