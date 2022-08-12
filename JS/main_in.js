f92291ec90d86ac9cd810afd99ac629564bfaf8e
Samuel Gomez <sgomezp.dev@gmail.com>
2022-08-12 16:37:13 -0600

    Se termino boton para ir arriba, se configuro contactame, se le agregara para que puedan descargar nuestro cv, sigue trabajar en footer

--- JS/main_in.js
+++ JS/main_in.js
@@ -3,11 +3,13 @@ const nav = document.querySelector('.nav-links');
 const navLinks = document.querySelectorAll('.nav-links li');
 const clickNav = document.querySelectorAll('.nav-links a[href^="#"]');
 
+const ScrollBtn = document.querySelector('.btn-start').style;
+
 //This is div container about, usage to set and get opacity
 const ScrollNavAbout = document.querySelector('.about');
 
 //This is div container Home
-const ContainerSpace = document.querySelector('.container-space'); 
+const ContainerSpace = document.querySelector('.container-space');
 //This is div container projects
 const ContainerProj = document.querySelector('.container-space-pj');
 //This is div container contact
@@ -16,8 +18,8 @@ const ContainerContact = document.querySelector('.Container-contact');
 const NumberInteractions = 7;
 const Seconds = 0.5;
 
-const Transition = (i,NumInt,Sc) => {
-     return i / NumInt + Sc;
+const Transition = (i, NumInt, Sc) => {
+    return i / NumInt + Sc;
 }
 
 const CalcTimeMov = 7 + 1.7;
@@ -26,9 +28,9 @@ const ScrollView = () => {
     var ScrollTop = document.documentElement.scrollTop;
     var Bottom = ContainerSpace.offsetHeight / 2;
     var IsOpacity = ScrollNavAbout.style.opacity;
-    
+
     //Visible About
-    ScrollNavAbout.style.opacity = IsOpacity == 1 ? 1 : ScrollTop >= Bottom ? 1 : 0; 
+    ScrollNavAbout.style.opacity = IsOpacity == 1 ? 1 : ScrollTop >= Bottom ? 1 : 0;
     ScrollNavAbout.classList.add('viewTop');
     IsOpacity = 0;
     Bottom = 0;
@@ -45,6 +47,7 @@ const ScrollView = () => {
     ContainerContact.style.opacity = IsOpacity == 1 ? 1 : ScrollTop >= Bottom ? 1 : 0;
     ContainerContact.classList.add('viewTop');
     console.log(ScrollTop);
+    ScrollTop > 400 ? ScrollBtn.opacity = 1 : ScrollBtn.opacity = 0;
 }
 
 const GetSize = () => {
@@ -56,25 +59,25 @@ const GetSize = () => {
     console.log(content);
 }
 
-const navSlide = () => { 
+const navSlide = () => {
     Burguer.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
-        navLinks.forEach((item,index) => {
+        navLinks.forEach((item, index) => {
             item.style.animation ?
-            item.style.animation = '' :
-            item.style.animation = `navLinkMovements 0.5s ease forwards ${Transition(index,NumberInteractions,Seconds)}s`;
+                item.style.animation = '' :
+                item.style.animation = `navLinkMovements 0.5s ease forwards ${Transition(index,NumberInteractions,Seconds)}s`;
             //console.log(index / CalcTimeMov);
         });
         Burguer.classList.toggle('toggle');
-    });     
+    });
 }
 
 const OnClick = () => {
     clickNav.forEach(links => {
-        links.addEventListener('click',() => {
+        links.addEventListener('click', () => {
             nav.classList.remove('nav-active');
             Burguer.classList.remove('toggle');
-            navLinks.forEach((item,index) => {
+            navLinks.forEach((item, index) => {
                 item.style.animation = '';
             });
         });
@@ -89,10 +92,11 @@ const OnLoad = () => {
     ScrollNavAbout.style.opacity = 0;
     ContainerProj.style.opacity = 0;
     ContainerContact.style.opacity = 0;
+    ScrollBtn.opacity = 0;
 }
 
-window.addEventListener('load',OnLoad)
-window.addEventListener('scroll',ScrollView)
+window.addEventListener('load', OnLoad)
+window.addEventListener('scroll', ScrollView)
 
 GetSize();
 navSlide();
